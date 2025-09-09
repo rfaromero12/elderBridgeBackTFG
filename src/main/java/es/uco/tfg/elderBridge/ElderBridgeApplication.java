58286d4e642d47bc3e9089e56d99ea9655e0a6b9
rfaromero12_ <rafael24.07.2001@gmail.com>
2025-09-09 18:49:34 +0200

    corregir paquete

--- src/main/java/es/uco/tfg/elderBridge/ElderBridgeApplication.java
+++ src/main/java/es/uco/tfg/elderBridge/ElderBridgeApplication.java
@@ -1,4 +1,4 @@
-package com.uco.tfg.elderBridge;
+package es.uco.tfg.elderBridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
